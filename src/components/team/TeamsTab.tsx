68ed045a41b6efa782c09c0a67b4037385200d75
Lonkel <linuscollin23@gmail.com>
2025-06-15 23:24:49 +0200

    ui 2

--- src/components/team/TeamsTab.tsx
+++ src/components/team/TeamsTab.tsx
@@ -4,7 +4,6 @@
 import { useRun, Status } from '@/context/RunContext'
 import type { Encounter } from '@/context/RunContext'
 
-import TypeIcon   from '@/components/ui/TypeIcon'
 import TypeGrid   from '@/components/TypeGrid'          // ← NEU
 import { sprite } from '@/utils/sprites'
 import { pokemonTypes } from '@/utils/pokemonTypes'
